2ad57a52e72c7470b453cfcac8d0882a8abccf57
DongHoon Lee <dhl1924@naver.com>
2025-07-16 20:37:12 +0900

    feat: admin, apis 모듈에 도메인 스캔 범위 및 JPA 설정 제거
    - JpaConfig에서 jpa 설정 통합 관리

--- ringle-admin/src/main/java/org/ringle/admin/RingleAdminApplication.java
+++ ringle-admin/src/main/java/org/ringle/admin/RingleAdminApplication.java
@@ -2,8 +2,18 @@ package org.ringle.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {
+		"org.ringle.admin",
+		"org.ringle.infra",
+		"org.ringle.domain",
+		"org.ringle.gateway",
+		"org.ringle.globalutils"
+	},
+	exclude = {JpaRepositoriesAutoConfiguration.class}
+)
 public class RingleAdminApplication {
 
 	public static void main(String[] args) {
--- ringle-apis/src/main/java/org/ringle/apis/RingleApisApplication.java
+++ ringle-apis/src/main/java/org/ringle/apis/RingleApisApplication.java
@@ -2,8 +2,18 @@ package org.ringle.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {
+		"org.ringle.apis",
+		"org.ringle.infra",
+		"org.ringle.domain",
+		"org.ringle.gateway",
+		"org.ringle.globalutils"
+	},
+	exclude = {JpaRepositoriesAutoConfiguration.class}
+)
 public class RingleApisApplication {
 
 	public static void main(String[] args) {
