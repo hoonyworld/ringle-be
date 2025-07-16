4f495cbf1b5ea0aac81c1b21cd0ebd5b20c41d82
DongHoon Lee <dhl1924@naver.com>
2025-07-16 19:36:52 +0900

    init: 프로젝트 초기 세팅

---
+++ ringle-global-utils/src/main/java/org/ringle/globalutils/RingleGlobalUtilsApplication.java
@@ -0,0 +1,13 @@
+package org.ringle.globalutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingleGlobalUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingleGlobalUtilsApplication.class, args);
+	}
+
+}
---
+++ ringle-global-utils/src/test/java/org/ringle/globalutils/RingleGlobalUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ringle.globalutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingleGlobalUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
