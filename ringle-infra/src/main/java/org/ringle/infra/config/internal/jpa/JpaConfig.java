68da2da4cf6aaa8730db7bf9928839c8dc4bde7f
DongHoon Lee <dhl1924@naver.com>
2025-07-18 23:19:31 +0900

    feat: EnableJpaAuditing 어노테이션 추가

--- ringle-infra/src/main/java/org/ringle/infra/config/internal/jpa/JpaConfig.java
+++ ringle-infra/src/main/java/org/ringle/infra/config/internal/jpa/JpaConfig.java
@@ -3,13 +3,15 @@ package org.ringle.infra.config.internal.jpa;
 import org.ringle.infra.InfraBaseConfig;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaAuditing
 @EntityScan(basePackages = "org.ringle.domain")
-@EnableJpaRepositories(basePackages = "org.ringle.domain")
+@EnableJpaRepositories(basePackages = "org.ringle")
 public class JpaConfig implements InfraBaseConfig {
 }
 
